9435dbac1f156826c3754d788a8275bd21a7df16
quasi-presence <quasi_presence@yahoo.com>
2023-05-20 15:23:25 -0700

    Add navbar to user layout

--- src/layouts/UserLayout.vue
+++ src/layouts/UserLayout.vue
@@ -1,10 +1,14 @@
 <template>
     <div>
       <UserSidebar />
-      <slot></slot>
+      <div class="ml-auto mb-6 lg:w-[75%] xl:w-[80%] 2xl:w-[85%]">
+        <UserNavbar />
+        <slot></slot>
+      </div>
     </div>
 </template>
 
 <script setup lang="ts">
   import UserSidebar from '../components/UserSidebar.vue';
+  import UserNavbar from '../components/UserNavbar.vue';
 </script>
